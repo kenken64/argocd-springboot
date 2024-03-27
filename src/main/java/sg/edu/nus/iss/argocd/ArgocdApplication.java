658d399e4df8a994b3ea990419f43e93d27b00a3
KENNETH PHANG <kennethphang@KENNETHs-MacBook-Pro.local>
2024-03-27 14:13:56 +0800

    upd

--- src/main/java/sg/edu/nus/iss/argocd/ArgocdApplication.java
+++ src/main/java/sg/edu/nus/iss/argocd/ArgocdApplication.java
@@ -1,13 +1,26 @@
 package sg.edu.nus.iss.argocd;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class ArgocdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArgocdApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ArgocdApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner runner(){
+        return args -> {
+              log.info("Application Version: V1");
+			  //System.out.println("Application Version: V1");
+        };
+    }
 
 }
